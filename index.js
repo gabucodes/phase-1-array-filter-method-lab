99542bd494a953e6babdb45a76df4f551ef08358
Gabriel <kipkoechgabriel27@gmail.com>
2025-03-13 08:39:47 +0300

    completes filter

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+function findMatching(drivers, searchString) {
+const lowerSearchString = searchString.toLowerCase()
+return drivers.filter(driver => driver.toLowerCase().includes(lowerSearchString)
+)
+}
+
+function fuzzyMatch(drivers, searchString) {
+    // Convert the search string to lowercase for case-insensitive comparison
+    const lowerSearchString = searchString.toLowerCase();
+
+    // Filter the drivers array
+    return drivers.filter(driver => 
+        driver.toLowerCase().startsWith(lowerSearchString)
+    );
+}
+
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
